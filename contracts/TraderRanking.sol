d9e5046bcfd313efe50c6d5b6c8eb34c47e55f6a
1up8192 <1up8192@gmail.com>
2017-11-29 18:02:25 +0100

    Create TraderRanking.sol

---
+++ contracts/TraderRanking.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.18;
+
+
+contract TraderRanking {
+    
+    mapping (address => uint) ranking;
+    
+    function getMaxAvailableFunds(address user) public constant;
+    
+    function getMaxAvailableLength(address user) public constant;
+    
+    function adjustRanking() internal;
+    
+}
