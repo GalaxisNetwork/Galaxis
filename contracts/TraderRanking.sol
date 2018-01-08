156aaf3765ce9a626ef75f5efeb1e597151fce88
1up8192 <1up8192@gmail.com>
2018-01-08 18:11:41 +0100

    Update TraderRanking.sol
    simple ranking algorythm

--- contracts/TraderRanking.sol
+++ contracts/TraderRanking.sol
@@ -2,13 +2,33 @@ pragma solidity ^0.4.18;
 
 
 contract TraderRanking {
-
-    mapping (address => uint) ranking;
+    
+    mapping (address => int) score;
+    mapping (address => uint8) negativeStreak; //number of last consecutive negative trades (0 if last trade was positive)
+    
+    uint summaryOfAllScores;
 
     function getMaxAvailableFunds(address user) public constant;
-
+    
     function getMaxAvailableLength(address user) public constant;
-
-    function adjustRanking(int rankingChange) internal;
-
+    
+    function calculateScore(address trader, int lastTradeProfit) internal { //lastTradeProfit can be nagative
+        if(lastTradeProfit < 0){
+            negativeStreak[trader] += 1;
+        } else {
+            negativeStreak[trader] = 0;
+        }
+        score[trader] += lastTradeProfit * (negativeStreak[trader] + 1);
+    }
+    
+    function dismissTrader(address trader) private {
+        delete score[trader];
+        delete negativeStreak[trader];
+    }
+    
+    function addTrader(address trader) private {
+        score[trader] = 1;
+        summaryOfAllScores += 1;
+    }
+    
 }
