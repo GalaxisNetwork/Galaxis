1b7f79d58747aa852253aba43b4bad6e6c1e415e
1up8192 <1up8192@gmail.com>
2018-01-08 18:12:18 +0100

    Update GeneralExchangeInterface.sol
    check order status method added

--- contracts/GeneralExchangeInterface.sol
+++ contracts/GeneralExchangeInterface.sol
@@ -2,9 +2,11 @@ pragma solidity ^0.4.18;
 
 
 interface ExchangeInterface {
-
-    function submitOrder(address tokenAddressToGive, uint tokenAtomValueToGive, address tokenAddressToGet, uint tokenAtomValueToGet) public;
-
+    
+    function submitOrder(address tokenAddressToGive, uint tokenAtomValueToGive, address tokenAddressToGet, uint tokenAtomValueToGet) public returns (bytes32 OrderID);
+    
+    function checkOrderStatus(bytes32 orderID) public returns (uint8 status);
+    
     function cancelOrder(bytes32 orderID) public;
-
+    
 }
